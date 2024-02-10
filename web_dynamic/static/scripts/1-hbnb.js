43c7f59fd54672935e1148d6bec6874e124cadd6
Bakii <bakii@ubutnu.ubuntu.virtualbox.org>
2024-02-10 14:15:12 +0300

    chechs

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,25 @@
-$('document').read(function(){
-    alert('try');
+
+$(document).ready(function(){
+    let checked_dict = {};
+    $('.chlist').change(function(){
+        //console.log('clicked');
+        let name = $(this).data('name');
+        let ids = $(this).data('id');
+        
+        if ($(this).prop('checked')){
+            checked_dict[ids] = name;
+        } else {
+            delete checked_dict[ids];
+        }
+        let txt = ''
+        for (var key in checked_dict) {
+            var value = checked_dict[key];
+            if (txt === ''){
+                txt = txt + value;
+            } else {
+                txt = txt + ', ' + value;
+            }
+        }
+        $('#h4tag').text(txt);
+    });
 });
