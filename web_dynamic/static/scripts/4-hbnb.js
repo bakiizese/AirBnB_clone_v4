c7de52d16b0bb61232cac628288d2c45fbc8b317
Bakii <bakii@ubutnu.ubuntu.virtualbox.org>
2024-02-11 13:45:41 +0300

    fix

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    let checked_dict = {};
+    $('.chlist').change(function(){
+        //console.log('clicked');
+        let name = $(this).data('name');
+        let ids = $(this).data('id');
+        
+        if ($(this).prop('checked')){
+            checked_dict[ids] = name;
+        } else {
+            delete checked_dict[ids];
+        }
+        let txt = ''
+        for (let key in checked_dict) {
+            let value = checked_dict[key];
+            if (txt === ''){
+                txt = txt + value;
+            } else {
+                txt = txt + ', ' + value;
+            }
+        }
+        $('#h4tag').text(txt);
+    });
+    $.ajax({
+        methods: 'GET',
+        url: 'http://0.0.0.0:5001/api/v1/status/',
+    }).done(function(data){
+        if (data.status == 'OK'){
+            $('div#api_status').addClass('available');
+        } else {
+            $('div#api_status').removeClass('available');
+        }
+    });
+    function searchs(srch = {}) {
+        console.log(srch);
+        $.ajax({
+            method: 'POST',
+            url: 'http://0.0.0.0:5001/api/v1/places_search',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            //'{"amenities": ["20f281b1-2cd1-4e36-a7c7-d1062ff16bcd"]}'
+            data: JSON.stringify(srch),
+            success: function(response){
+                for (j in response) {
+                    let p = response[j];
+                    $('.places').append('<article><div class="title_box"><h2>' + p.name + '</h2><div class="price_by_night">$' + p.price_by_night + '</div></div>\
+                    <div class="information"><div class="max_guest">' + p.max_guest + ' Guest' + (p.max_guest > 1 ? 's' : '') + '</div>\
+                    <div class="number_rooms">' + p.number_rooms + ' Bedroom' + (p.number_rooms > 1 ? 's': '') + '</div>\
+                    <div class="number_bathrooms">' + p.number_bathrooms + ' Bathroom' + (p.number_bathrooms > 1 ? 's' : '') + '</div>\
+                    </div><div class="user"><b>Owner:</b></div><div class="description">' + p.description + '</div></article>');
+                    /*
+                    for (let k in response){
+                        let v = response[k];
+                        for (let i in v){
+                            console.log(v[i]);
+                            
+                        }
+                        break;
+
+                        //console.log(k);
+                    }
+                    */
+                }
+                $('.places').addClass('places');
+                $('.title_box').addClass('title_box');
+                $('article .price_by_night').addClass('price_by_night');
+                $('article .max_guest').addClass('max_guest');
+                $('article .information').addClass('information');
+                $('.user').addClass('user');
+                $('article .number_rooms').addClass('number_rooms');
+                $('article .number_bathrooms').addClass('number_bathrooms');
+                $('article .description').addClass('description');
+
+            }
+        });
+    }
+    searchs();
+    $('button').click(function(){
+        let jamdict= {}
+        let amlist = []
+        for (let key in checked_dict) {
+            let value = checked_dict[key];
+            amlist.push(key);
+        }
+        jamdict['amenities'] = amlist;
+        searchs(jamdict);
+        //for (let key in jamdict){
+            //console.log(jamdict[key]);
+        //}
+    });
+});
